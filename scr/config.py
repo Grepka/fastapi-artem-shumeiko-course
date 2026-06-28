5896e1ab23157bf23fd591d233a5ee9a45571ecb
Grepka <grepkalab@yandex.ru>
2026-06-28 20:48:16 +0300

    feat: add database settings

---
+++ scr/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int = 5432
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    @property
+    def get_db_url(self):
+        return (f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+                f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}")
+
+settings = Settings()
